cc65066893c0ad23259e1871ae4d6d95e53f1d2b
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-04-27 16:51:24 +0100

    getters and setters

--- java/src/ie/tudublin/Nematode.java
+++ java/src/ie/tudublin/Nematode.java
@@ -1,17 +1,38 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+import processing.data.TableRow;
 
-public class Nematode extends PApplet {
+public class Nematode  extends PApplet{
+
     private String name;
+    private int lenght;
+    private boolean limbs;
+    private String gender;
+    private boolean eyes;
+
+
+    public Nematode(TableRow tr)
+    {
+        this(tr.getString("name"), 
+            tr.getInt("lenght"), 
+            tr.getInt("limbs") == 1, 
+            tr.getString("gender"), 
+            tr.getInt("eyes") == 1);
+    }
+
+    public Nematode(String name, int lenght, boolean limbs, String gender, boolean eyes) {
+        this.name = name;
+        this.lenght = lenght;
+        this.limbs = limbs;
+        this.gender = gender;
+        this.eyes = eyes;
+    }
     @Override
     public String toString() {
         return "Nematode [eyes=" + eyes + ", gender=" + gender + ", lenght=" + lenght + ", limbs=" + limbs + ", name="
                 + name + "]";
     }
-    private int lenght;
-    private boolean limbs;
-    private char gender;
     public String getName() {
         return name;
     }
@@ -30,10 +51,10 @@ public class Nematode extends PApplet {
     public void setLimbs(boolean limbs) {
         this.limbs = limbs;
     }
-    public char getGender() {
+    public String getGender() {
         return gender;
     }
-    public void setGender(char gender) {
+    public void setGender(String gender) {
         this.gender = gender;
     }
     public boolean isEyes() {
@@ -42,5 +63,5 @@ public class Nematode extends PApplet {
     public void setEyes(boolean eyes) {
         this.eyes = eyes;
     }
-    private boolean eyes;
+
 }
